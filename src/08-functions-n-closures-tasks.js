939fdffb353a54b9b8379e8f896a3fa907bc074f
Valery91-qw <vakriv91@gmail.com>
2022-01-18 15:32:03 +0300

    feat: complete more task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -147,8 +147,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  if (fn.length === args1.length) {
+    return () => fn(...args1);
+  }
+  return (...args) => fn(...args1, ...args);
 }
 
 /**
